c3b7a31ee7659d0217a79262e21b5ae1cb745340
IlPakoZ <pasquale.lobascioo@gmail.com>
2025-11-01 21:42:44 +0000

    Deploying to gh-pages from @ IlPakoZ/ilpakoz.github.io@7d5d03829596acf4ca643c079f0db7eafab7fc38 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -37,12 +37,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
+        },{id: "nav-bookshelf",
+          title: "bookshelf",
           description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
+          },
+        },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
